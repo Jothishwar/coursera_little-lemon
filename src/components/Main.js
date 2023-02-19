a7cc16e8fbe5a114934eac94345d946ef3361d46
jothishwar <jothishwarvvr@gmail.com>
2023-02-19 11:51:47 +0530

    added other pages

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./home/Home";
-// import About from "../About";
-// import Booking from "../booking/Booking";
-// import OrderOnline from "../OrderOnline";
-// import TodayMenu from "../TodayMenu";
-// import Login from "../Login";
-// import ConfirmedBooking from "../booking/ConfirmedBooking";
+import About from "./about/About";
+import Booking from "../booking/Booking";
+import OrderOnline from "./order/OrderOnline";
+import TodayMenu from "./menu/TodayMenu";
+import Login from "./login/Login";
+import ConfirmedBooking from "../booking/ConfirmedBooking";
 
 const Main = () => {
   return (
     <main>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/*<Route path="/about" element={<About />} />
-                <Route path="/booking" element={<Booking />} />
-                <Route path="/comfirmedb" element={<ConfirmedBooking />} />
-                <Route path="/order" element={<OrderOnline />} />
-                <Route path="/menu" element={<TodayMenu />} />
-                <Route path="/login" element={<Login />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/comfirmedb" element={<ConfirmedBooking />} />
+        <Route path="/order" element={<OrderOnline />} />
+        <Route path="/menu" element={<TodayMenu />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </main>
   );
